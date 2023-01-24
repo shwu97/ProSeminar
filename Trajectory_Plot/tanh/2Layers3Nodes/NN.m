00e5bc4113f28ef83cc0e5fdf88ea3e0807871d3
shwu97 <123260199+shwu97@users.noreply.github.com>
2023-01-24 12:56:04 +0100

    Add files via upload

---
+++ Trajectory_Plot/tanh/2Layers3Nodes/NN.m
@@ -0,0 +1,20 @@
+function u = NN(t, z, W0, W1, W2, b0, b1, b2)
+    h0 = arrayfun(@Tanh,(W0*z+transpose(b0)));
+    h1 = arrayfun(@Tanh,(W1*h0+transpose(b1)));
+    %h2 = arrayfun(@Tanh,(W2*h1+transpose(b2)));
+    u = arrayfun(@Tanh,(W2*h1+b2));
+
+% beg0 =-2;
+% end0 = 2;
+% steps = 1;
+% for z10 = beg0:steps:end0
+%     for z20 = beg0:steps:end0
+%         for z30 = beg0:steps:end0
+%             z=[z10;z20;z30];
+%             h0 = arrayfun(@ReLu,(W0*z+transpose(b0)));
+%             h1 = arrayfun(@ReLu,(W1*h0+transpose(b1)));
+%             u = arrayfun(@ReLu,(W2*h1+b2))
+%         end
+%     end
+% end
+end
